882d9aa52eea568c7d956bdb7c06f096a16c6226
Thomas.Finnern <GitProjects@insidethemachine.de>
2015-05-26 21:00:53 +0200

    Upgrade from J2.5
    Initial Version for Joomla 3x

---
+++ changelog.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* @version $Id:$
+* @package rsgallery2_gallery_creator.php
+* @copyright Copyright (C) 2007 - 2015 RSGallery2 Team. (Code changed for Joomla 2.5 and addition of clearfloat and modal behaviour)
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* 
+*/
+defined( '_JEXEC' ) or die();
+?>
+
+Changelog
+------------
+This is a non-exhaustive but informative changelog for the rsgallery2_gallery_creator 
+plugin for RSGallery2, including alpha, beta and stable releases.
+Our thanks to all those people who've contributed bug reports and code fixes.
+
+Legend:
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+	
+2015-03-21 finnern - SVN ...
++ Adapted for J!3.x  
+	
---
+++ rsgallery2_gallery_creator.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * RSGallery2 Gallery Creator plugin
+ * This plugin creates a new gallery for a newly created user
+ *
+ * @version      $Id: rsgallery2_gallery_creator.php  $
+ * @package		RSGallery2
+ * @subpackage	Content plugin
+ * @copyright	Copyright (C) 2013 - 2015 RSGallery2
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * RSGallery is Free Software
+ */
+
+// No direct access
+defined('JPATH_BASE') or die;
+
+/**
+ * @package		Joomla.Plugin
+ * @subpackage	User.RSGallery2_gallery_creator
+ */
+class plgUserRsgallery2_gallery_creator extends JPlugin{
+
+    /**
+     * Load the language file on instantiation. Note this is only available in Joomla 3.1 and higher.
+     * If you want to support 3.0 series you must override the constructor
+     *
+     * @var    boolean
+     * @since  3.1
+     */
+    protected $autoloadLanguage = true;
+
+	/**
+	 * Utility method to act on a user after it has been saved.
+	 *
+	 * Creates a gallery for RSGallery2 owned by the newly created user
+	 *
+	 * @param	array		$user		Holds the new user data.
+	 * @param	boolean		$isnew		True if a new user is stored.
+	 * @param	boolean		$success	True if user was succesfully stored in the database.
+	 * @param	string		$msg		Message. (don't know what kind of message)
+	 *
+	 * @return	void
+	 */
+	public function onUserAfterSave($user, $isnew, $success, $msg)
+	{
+		try {
+			// Initialise variables.
+			$database = JFactory::getDBO();
+			//$app	= JFactory::getApplication();
+			//$app->isAdmin() is true when user created in backend, false when created in frontend
+			
+			// Plugin parameters
+			$publish_gallery = $this->params->get('publish_gallery', 0);
+			$parent_gallery = $this->params->get('gid', 0);
+			
+			// Only create a gallery for a new user
+			if ($isnew) {
+				//// Load user_joomla plugin language (not done automatically).
+				//$lang = JFactory::getLanguage();
+				//$lang->load('plg_user_rsgallery2_gallery_creator', JPATH_ADMINISTRATOR);
+
+				// Initialise RSGallery2 component
+				require_once(JPATH_BASE.'/administrator/components/com_rsgallery2/init.rsgallery2.php');
+				require_once(JPATH_BASE.'/administrator/components/com_rsgallery2/options/galleries.class.php');//needed for new rsgGalleriesItem class
+
+				// Create the set of details for our new gallery
+				$row = new rsgGalleriesItem( $database );
+				$row->parent = $parent_gallery; // root for now
+				$row->name = $user['username'];
+				$row->alias = JFilterOutput::stringURLSafe($row->name);
+				$row->published = $publish_gallery;
+				$row->date = date( 'Y-m-d H:i:s' );
+				$row->ordering = ($row->getNextOrder('parent = '. $row->parent));
+				$row->uid = $user['id'];
+
+				// How to handle the asset/permissions? Todo: (now asset from parent is used automatically)
+				// Asset example:
+				//	{"core.create":[],"rsgallery2.create.own":[],"core.delete":[],"rsgallery2.delete.own":[],"core.edit":[],"core.edit.own":[],"core.edit.state":[],"rsgallery2.edit.state.own":[],"rsgallery2.vote":[],"rsgallery2.comment":[]}
+
+				if(!$row->check()) {
+					// JError::raiseWarning(500, JText::_('PLG_USER_RSGALLERY2_GALLERY_CREATOR_CHECK_FAILED'));
+					$msg = JText::_('PLG_USER_RSGALLERY2_GALLERY_CREATOR_CHECK_FAILED');
+					$app->enqueueMessage($msg, 'error');
+					return false;
+				}
+				if (!$row->store()) {
+					//JError::raiseWarning(500, JText::_('PLG_USER_RSGALLERY2_GALLERY_CREATOR_STORE_FAILED'));
+					$msg = JText::_('PLG_USER_RSGALLERY2_GALLERY_CREATOR_STORE_FAILED');
+					$app->enqueueMessage($msg, 'error');
+					return false;
+				}
+				
+				$success = JText::sprintf('PLG_USER_RSGALLERY2_GALLERY_CREATOR_SUCCESS', $user['username'] );
+				JFactory::getApplication()->enqueueMessage($success, 'message');
+				
+				$row->checkin();
+				$row->reorder( );
+			} else {
+				// Existing user - nothing to do...yet.
+				// ToDo: Perhaps check if the user owns a gallery, if not, create it ?
+			}
+		}
+		catch(Exception $e) {
+			$msg = JText::_('PLG_USER_RSGALLERY2_GALLERY_CREATOR') . ' Error:  (01)' . $e->getMessage();
+			$app->enqueueMessage($msg,'error');
+		}		
+	}
+
+	/**
+	 * Does nothing (yet): ToDo: perhaps do something with the user's gallery after the user is deleted?
+	 *
+	 * Method is called after user data is deleted from the database
+	 *
+	 * @param	array		$user	Holds the user data
+	 * @param	boolean		$succes	True if user was succesfully stored in the database
+	 * @param	string		$msg	Message
+	 *
+	 * @return	boolean
+	 */
+/*	public function onUserAfterDelete($user, $succes, $msg)
+	{
+		//Perhaps unpublish or delete the gallery belonging to the user?
+		//Gallery name is the username: $user[username]
+		JFactory::getApplication()->enqueueMessage('Message: user deleted');
+		return true;
+	}
+*/
+}
